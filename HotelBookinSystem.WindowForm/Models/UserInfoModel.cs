dd950a17693d0c143c81ac7329e51f64ac36b828
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-29 00:10:47 +0630

    -

--- HotelBookinSystem.WindowForm/Models/UserInfoModel.cs
+++ HotelBookinSystem.WindowForm/Models/UserInfoModel.cs
@@ -1,9 +1,8 @@
-﻿namespace HotelBookinSystem.WindowForm.Models
+﻿namespace HotelBookinSystem.WindowForm.Models;
+
+public class UserInfoModel
 {
-    public class UserInfoModel
-    {
-        public string UserId { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
+    public string UserId { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
 }
