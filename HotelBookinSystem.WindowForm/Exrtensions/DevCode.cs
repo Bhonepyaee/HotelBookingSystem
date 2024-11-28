3fa222c67aab49d5274aa35e69183a37c7d07425
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-29 00:10:29 +0630

    -

--- HotelBookinSystem.WindowForm/Exrtensions/DevCode.cs
+++ HotelBookinSystem.WindowForm/Exrtensions/DevCode.cs
@@ -1,13 +1,12 @@
 ﻿using Newtonsoft.Json;
 
-namespace HotelBookinSystem.WindowForm.Exrtensions
+namespace HotelBookinSystem.WindowForm.Exrtensions;
+
+public static class DevCode
 {
-    public static class DevCode
-    {
-        public static bool IsNullOrEmpty(this string str) => string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
+    public static bool IsNullOrEmpty(this string str) => string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
 
-        public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj, Formatting.Indented);
+    public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj, Formatting.Indented);
 
-        public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
-    }
+    public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
 }
