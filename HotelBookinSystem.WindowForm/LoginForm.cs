9397fbb05b6c65d4472ea7e658b9169de55bd4fd
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-29 00:08:32 +0630

    -

--- HotelBookinSystem.WindowForm/LoginForm.cs
+++ HotelBookinSystem.WindowForm/LoginForm.cs
@@ -4,85 +4,84 @@ using HotelBookinSystem.WindowForm.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 
-namespace HotelBookinSystem.WindowForm
+namespace HotelBookinSystem.WindowForm;
+
+public partial class LoginForm : Form
 {
-    public partial class LoginForm : Form
+    internal readonly AppDbContext _context;
+    internal readonly string _path;
+
+    public LoginForm(AppDbContext context)
     {
-        internal readonly AppDbContext _context;
-        internal readonly string _path;
+        InitializeComponent();
+        _context = context;
+        _path = Path.Combine(Directory.GetCurrentDirectory(), "Data/UserInfo.json");
+    }
 
-        public LoginForm(AppDbContext context)
+    private async void btnLogin_Click(object sender, EventArgs e)
+    {
+        try
         {
-            InitializeComponent();
-            _context = context;
-            _path = Path.Combine(Directory.GetCurrentDirectory(), "Data/UserInfo.json");
-        }
+            string email = txtEmail.Text.Trim();
+            string password = txtPassword.Text.Trim();
+            bool isRememberMeChecked = cboRememberMe.Checked;
 
-        private async void btnLogin_Click(object sender, EventArgs e)
-        {
-            try
+            if (!email.IsNullOrEmpty() && !password.IsNullOrEmpty())
             {
-                string email = txtEmail.Text.Trim();
-                string password = txtPassword.Text.Trim();
-                bool isRememberMeChecked = cboRememberMe.Checked;
+                var item = await _context.TblAdmins
+                    .FirstOrDefaultAsync(x => x.Email == email);
 
-                if (!email.IsNullOrEmpty() && !password.IsNullOrEmpty())
+                if (item is null)
                 {
-                    var item = await _context.TblAdmins
-                        .FirstOrDefaultAsync(x => x.Email == email);
-
-                    if (item is null)
-                    {
-                        MessageBox.Show("Login Fail.", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
-                        return;
-                    }
-
-                    if (item.FailCount >= 3)
-                    {
-                        MessageBox.Show("Account Locked.", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
-                        return;
-                    }
-
-                    if (!item.Password.Equals(password))
-                    {
-                        MessageBox.Show("Incorrect Password.", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
-
-                        item.FailCount += 1;
-                        _context.TblAdmins.Update(item);
-                        await _context.SaveChangesAsync();
-
-                        return;
-                    }
-
-                    if (isRememberMeChecked)
-                    {
-                        var userInfo = new UserInfoModel()
-                        {
-                            UserId = item.UserId,
-                            Email = item.Email,
-                            Password = item.Password
-                        };
-                        await File.WriteAllTextAsync(_path, userInfo.ToJson());
-                    }
-
-                    MessageBox.Show("Login Successful.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("Login Fail.", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                    return;
                 }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
+
+                if (item.FailCount >= 3)
+                {
+                    MessageBox.Show("Account Locked.", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (!item.Password.Equals(password))
+                {
+                    MessageBox.Show("Incorrect Password.", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+
+                    item.FailCount += 1;
+                    _context.TblAdmins.Update(item);
+                    await _context.SaveChangesAsync();
+
+                    return;
+                }
+
+                if (isRememberMeChecked)
+                {
+                    var userInfo = new UserInfoModel()
+                    {
+                        UserId = item.UserId,
+                        Email = item.Email,
+                        Password = item.Password
+                    };
+                    await File.WriteAllTextAsync(_path, userInfo.ToJson());
+                }
+
+                MessageBox.Show("Login Successful.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
-
-        private async void LoginForm_Load(object sender, EventArgs e)
+        catch (Exception ex)
         {
-            string jsonStr = await File.ReadAllTextAsync(_path);
-            var userInfoModel = jsonStr.ToObject<UserInfoModel>();
-            if (userInfoModel is not null)
-            {
-                txtEmail.Text = userInfoModel.Email;
-                txtPassword.Text = userInfoModel.Password;
-            }
+            throw new Exception(ex.Message);
+        }
+    }
+
+    private async void LoginForm_Load(object sender, EventArgs e)
+    {
+        string jsonStr = await File.ReadAllTextAsync(_path);
+        var userInfoModel = jsonStr.ToObject<UserInfoModel>();
+        if (userInfoModel is not null)
+        {
+            txtEmail.Text = userInfoModel.Email;
+            txtPassword.Text = userInfoModel.Password;
         }
     }
 }
