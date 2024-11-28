4467806a58d937682d53387519fb4a53c126e94c
Lin Thit Htoo <lth1212001@gmail.com>
2024-11-29 00:10:36 +0630

    -

--- HotelBookinSystem.WindowForm/Exrtensions/DevCode.cs
+++ HotelBookinSystem.WindowForm/Exrtensions/DevCode.cs
@@ -4,9 +4,11 @@ namespace HotelBookinSystem.WindowForm.Exrtensions;
 
 public static class DevCode
 {
-    public static bool IsNullOrEmpty(this string str) => string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
+    public static bool IsNullOrEmpty(this string str) =>
+        string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
 
-    public static string ToJson(this object obj) => JsonConvert.SerializeObject(obj, Formatting.Indented);
+    public static string ToJson(this object obj) =>
+        JsonConvert.SerializeObject(obj, Formatting.Indented);
 
     public static T ToObject<T>(this string jsonStr) => JsonConvert.DeserializeObject<T>(jsonStr)!;
 }
