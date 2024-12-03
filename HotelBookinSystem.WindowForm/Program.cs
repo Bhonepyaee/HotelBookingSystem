3783126aa6250155696275fe476c0044e44be058
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-03 19:31:54 +0630

    -

--- HotelBookinSystem.WindowForm/Program.cs
+++ HotelBookinSystem.WindowForm/Program.cs
@@ -1,22 +1,21 @@
 using HotelBookinSystem.WindowForm.AppDbContextModels;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace HotelBookinSystem.WindowForm
+namespace HotelBookinSystem.WindowForm;
+
+internal static class Program
 {
-    internal static class Program
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            var serviceProvider = new ServiceCollection()
-                .AddDbContext<AppDbContext>()
-                .BuildServiceProvider();
-            var context = serviceProvider.GetRequiredService<AppDbContext>();
-            ApplicationConfiguration.Initialize();
-            Application.Run(new LoginForm(context));
-        }
+        var serviceProvider = new ServiceCollection()
+            .AddDbContext<AppDbContext>()
+            .BuildServiceProvider();
+        var context = serviceProvider.GetRequiredService<AppDbContext>();
+        ApplicationConfiguration.Initialize();
+        Application.Run(new LoginForm(context));
     }
 }
