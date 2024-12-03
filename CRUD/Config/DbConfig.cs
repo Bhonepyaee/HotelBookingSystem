014ca2e34129a7430b1a137f3149564fd5532c41
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-03 19:33:12 +0630

    -

--- CRUD/Config/DbConfig.cs
+++ CRUD/Config/DbConfig.cs
@@ -1,8 +1,7 @@
-﻿namespace CRUD.Config
+﻿namespace CRUD.Config;
+
+public class DbConfig
 {
-    public class DbConfig
-    {
-        public static string _connectionString { get; } =
-            "Server=.;Database=bhone pyae;User ID=sa;Password=sasa@123;TrustServerCertificate=True;";
-    }
+    public static string _connectionString { get; } =
+        "Server=.;Database=bhone pyae;User ID=sa;Password=sasa@123;TrustServerCertificate=True;";
 }
