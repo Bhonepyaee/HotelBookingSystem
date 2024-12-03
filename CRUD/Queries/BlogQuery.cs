c3a5a36999b28c287120ac023b0b4ec0c6123de5
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-03 19:33:32 +0630

    -

--- CRUD/Queries/BlogQuery.cs
+++ CRUD/Queries/BlogQuery.cs
@@ -1,13 +1,12 @@
-﻿namespace CRUD.Queries
+﻿namespace CRUD.Queries;
+
+public class BlogQuery
 {
-    public class BlogQuery
-    {
-        public static string GetBlogListQuery { get; } =
-            @"SELECT BlogId,BlogTitle,BlogAuthor,BlogContent 
+    public static string GetBlogListQuery { get; } =
+        @"SELECT BlogId,BlogTitle,BlogAuthor,BlogContent 
               FROM Tbl_Blog ORDER BY BlogId DESC";
 
-        public static string UpdateBlogQuery { get; } =
-            @"UPDATE Tbl_Blog SET BlogTitle = @BlogTitle, 
+    public static string UpdateBlogQuery { get; } =
+        @"UPDATE Tbl_Blog SET BlogTitle = @BlogTitle, 
          BlogAuthor= @BlogAuthor, BlogContent = @BlogContent WHERE BlogId = @BlogId";
-    }
 }
