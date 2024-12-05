dfd2f3ad301f8f484e7f9535c52dcc2f6b8f8739
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-05 20:29:12 +0630

    -

--- HotelBookinSystem.WindowForm/Forgot PSW.cs
+++ HotelBookinSystem.WindowForm/Forgot PSW.cs
@@ -4,68 +4,67 @@ using HotelBookinSystem.WindowForm.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace HotelBookinSystem.WindowForm
+namespace HotelBookinSystem.WindowForm;
+
+public partial class Forgot_PSW : Form
 {
-    public partial class Forgot_PSW : Form
+    internal readonly AppDbContext _context;
+    internal readonly string _subject;
+    internal readonly OtpService _otpService;
+
+    public Forgot_PSW()
     {
-        internal readonly AppDbContext _context;
-        internal readonly string _subject;
-        internal readonly OtpService _otpService;
+        InitializeComponent();
+        _context = Program.ServiceProvider.GetRequiredService<AppDbContext>();
+        _otpService = Program.ServiceProvider.GetRequiredService<OtpService>();
+        _subject = "Reset Password OTP Code";
+    }
 
-        public Forgot_PSW()
+    private async void btnSend_Click(object sender, EventArgs e)
+    {
+        try
         {
-            InitializeComponent();
-            _context = Program.ServiceProvider.GetRequiredService<AppDbContext>();
-            _otpService = Program.ServiceProvider.GetRequiredService<OtpService>();
-            _subject = "Reset Password OTP Code";
-        }
-
-        private async void btnSend_Click(object sender, EventArgs e)
-        {
-            try
+            string email = txtEmail.Text;
+            if (!email.IsNullOrEmpty())
             {
-                string email = txtEmail.Text;
-                if (!email.IsNullOrEmpty())
+                var user = await _context.TblAdmins.FirstOrDefaultAsync(x =>
+                    x.Email == email && !x.IsDeleted
+                );
+                if (user is null)
                 {
-                    var user = await _context.TblAdmins.FirstOrDefaultAsync(x =>
-                        x.Email == email && !x.IsDeleted
+                    MessageBox.Show(
+                        "Invalid Email",
+                        "Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
                     );
-                    if (user is null)
-                    {
-                        MessageBox.Show(
-                            "Invalid Email",
-                            "Error",
-                            MessageBoxButtons.OK,
-                            MessageBoxIcon.Error
-                        );
-                        return;
-                    }
-
-                    string otpValue = _otpService.GetSixDigitRandomNumber();
-                    string otpId = await _otpService.SendOtpViaEmailAsync(user.UserId, otpValue, user.Email, _subject);
-
-                    if (otpId.IsNullOrEmpty())
-                    {
-                        MessageBox.Show("Error occured.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        return;
-                    }
-
-                    OTP_CODE oTP_CODE = new(user.Email, user.UserId, otpId);
-                    oTP_CODE.Show();
-                    this.Hide();
+                    return;
                 }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
 
-        private string GetSixDigitRandomNumber()
-        {
-            Random r = new();
-            int randNum = r.Next(100000, 1000000);
-            return randNum.ToString();
+                string otpValue = _otpService.GetSixDigitRandomNumber();
+                string otpId = await _otpService.SendOtpViaEmailAsync(user.UserId, otpValue, user.Email, _subject);
+
+                if (otpId.IsNullOrEmpty())
+                {
+                    MessageBox.Show("Error occured.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                OTP_CODE oTP_CODE = new(user.Email, user.UserId, otpId);
+                oTP_CODE.Show();
+                this.Hide();
+            }
         }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    private string GetSixDigitRandomNumber()
+    {
+        Random r = new();
+        int randNum = r.Next(100000, 1000000);
+        return randNum.ToString();
     }
 }
