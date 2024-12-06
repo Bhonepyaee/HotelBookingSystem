38c9bc495d73377425c660c3b9cbc6b0c1377a46
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-06 08:50:54 +0630

    -

--- HotelBookinSystem.WindowForm/Services/OtpService.cs
+++ HotelBookinSystem.WindowForm/Services/OtpService.cs
@@ -2,56 +2,55 @@
 using HotelBookinSystem.WindowForm.AppDbContextModels;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace HotelBookinSystem.WindowForm.Services
+namespace HotelBookinSystem.WindowForm.Services;
+
+public class OtpService
 {
-    public class OtpService
+    internal readonly AppDbContext _context;
+    internal readonly IFluentEmail _email;
+
+    public OtpService(AppDbContext context)
     {
-        internal readonly AppDbContext _context;
-        internal readonly IFluentEmail _email;
+        _context = context;
 
-        public OtpService(AppDbContext context)
+        var serviceCollection = new ServiceCollection();
+        serviceCollection
+            .AddFluentEmail("lth1212001@gmail.com")
+            .AddSmtpSender(
+                "smtp.gmail.com",
+                587,
+                "lth1212001@gmail.com",
+                "evzd omax jylb pdzn"
+            );
+        var sericeProvider = serviceCollection.BuildServiceProvider();
+        var _fluentEmail = sericeProvider.GetRequiredService<IFluentEmail>();
+
+        _email = _fluentEmail;
+    }
+
+    public string GetSixDigitRandomNumber()
+    {
+        Random r = new();
+        int randNum = r.Next(100000, 1000000);
+        return randNum.ToString();
+    }
+
+    public async Task<string> SendOtpViaEmailAsync(string userId, string otpValue, string toEmail, string subject)
+    {
+        var otp = new TblOtp()
         {
-            _context = context;
+            Id = Guid.NewGuid().ToString(),
+            OtpValue = Convert.ToInt32(otpValue),
+            CreatedAt = DateTime.Now,
+            UserId = userId,
+            IsDeleted = false
+        };
+        await _context.TblOtps.AddAsync(otp);
+        await _context.SaveChangesAsync();
+        string body =
+            $"Here is your OTP Code: {otpValue}. This Code will be expired within 3 minutes.";
+        await _email.To(toEmail).Subject(subject).Body(body).SendAsync();
 
-            var serviceCollection = new ServiceCollection();
-            serviceCollection
-                .AddFluentEmail("lth1212001@gmail.com")
-                .AddSmtpSender(
-                    "smtp.gmail.com",
-                    587,
-                    "lth1212001@gmail.com",
-                    "evzd omax jylb pdzn"
-                );
-            var sericeProvider = serviceCollection.BuildServiceProvider();
-            var _fluentEmail = sericeProvider.GetRequiredService<IFluentEmail>();
-
-            _email = _fluentEmail;
-        }
-
-        public string GetSixDigitRandomNumber()
-        {
-            Random r = new();
-            int randNum = r.Next(100000, 1000000);
-            return randNum.ToString();
-        }
-
-        public async Task<string> SendOtpViaEmailAsync(string userId, string otpValue, string toEmail, string subject)
-        {
-            var otp = new TblOtp()
-            {
-                Id = Guid.NewGuid().ToString(),
-                OtpValue = Convert.ToInt32(otpValue),
-                CreatedAt = DateTime.Now,
-                UserId = userId,
-                IsDeleted = false
-            };
-            await _context.TblOtps.AddAsync(otp);
-            await _context.SaveChangesAsync();
-            string body =
-                $"Here is your OTP Code: {otpValue}. This Code will be expired within 3 minutes.";
-            await _email.To(toEmail).Subject(subject).Body(body).SendAsync();
-
-            return otp.Id;
-        }
+        return otp.Id;
     }
 }
