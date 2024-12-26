3c310d0be91f5be39aeea6d7994d8f7b0c17e89d
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-26 22:48:55 +0630

    -

--- HotelBookinSystem.WindowForm/Query/CustomerQuery.cs
+++ HotelBookinSystem.WindowForm/Query/CustomerQuery.cs
@@ -1,13 +1,12 @@
-﻿namespace HotelBookinSystem.WindowForm.Query
+﻿namespace HotelBookinSystem.WindowForm.Query;
+
+public class CustomerQuery
 {
-    public class CustomerQuery
-    {
-        public static string GetCustomerQuery { get; } =
-            @"SELECT CustomerId, FirstName, LastName, Phone, Email, Address 
+    public static string GetCustomerQuery { get; } =
+        @"SELECT CustomerId, FirstName, LastName, Phone, Email, Address 
              FROM Customer_Table ORDER BY CustomerId DESC";
 
-        public static string UpdateCustomerQuery { get; } =
-            @"UPDATE Customer_Table SET FirstName = @FirstName, LastName = @LastName,
+    public static string UpdateCustomerQuery { get; } =
+        @"UPDATE Customer_Table SET FirstName = @FirstName, LastName = @LastName,
               Phone = @Phone, Email = @Email, Address = @Address WHERE CustomerId = @CustomerId";
-    }
 }
