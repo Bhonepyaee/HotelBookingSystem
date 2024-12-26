b167f824f622451da326b30c41c6496e98b896e1
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-26 22:51:42 +0630

    -

--- HotelBookinSystem.WindowForm/RoomListForm.cs
+++ HotelBookinSystem.WindowForm/RoomListForm.cs
@@ -3,43 +3,42 @@ using System.Data.SqlClient;
 using HotelBookinSystem.WindowForm.Config;
 using HotelBookinSystem.WindowForm.Query;
 
-namespace HotelBookinSystem.WindowForm
+namespace HotelBookinSystem.WindowForm;
+
+public partial class RoomListForm : Form
 {
-    public partial class RoomListForm : Form
+    public RoomListForm()
     {
-        public RoomListForm()
+        InitializeComponent();
+    }
+
+    private async void RoomListForm_Load(object sender, EventArgs e)
+    {
+        try
         {
-            InitializeComponent();
-        }
+            string query = RoomQuery.GetRoomQuery;
+            SqlConnection connection = new SqlConnection(DbConfig._connectionString);
+            await connection.OpenAsync();
 
-        private async void RoomListForm_Load(object sender, EventArgs e)
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            await connection.CloseAsync();
+
+            dataGridView1.DataSource = dt;
+        }
+        catch (Exception ex)
         {
-            try
-            {
-                string query = RoomQuery.GetRoomQuery;
-                SqlConnection connection = new SqlConnection(DbConfig._connectionString);
-                await connection.OpenAsync();
-
-                SqlCommand command = new SqlCommand(query, connection);
-                SqlDataAdapter adapter = new SqlDataAdapter(command);
-
-                DataTable dt = new DataTable();
-                adapter.Fill(dt);
-                await connection.CloseAsync();
-
-                dataGridView1.DataSource = dt;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            throw new Exception(ex.Message);
         }
+    }
 
-        private void btn_create_Click(object sender, EventArgs e)
-        {
-            RoomForm roomForm = new RoomForm();
-            roomForm.Show();
-            this.Hide();
-        }
+    private void btn_create_Click(object sender, EventArgs e)
+    {
+        RoomForm roomForm = new RoomForm();
+        roomForm.Show();
+        this.Hide();
     }
 }
