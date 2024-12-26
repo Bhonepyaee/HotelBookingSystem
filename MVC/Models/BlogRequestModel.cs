72434cbe8539deec93847f18148cbef67e89a994
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-26 22:53:09 +0630

    -

--- MVC/Models/BlogRequestModel.cs
+++ MVC/Models/BlogRequestModel.cs
@@ -1,9 +1,8 @@
-﻿namespace MVC.Models
+﻿namespace MVC.Models;
+
+public class BlogRequestModel
 {
-    public class BlogRequestModel
-    {
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
