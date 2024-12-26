ec6392981d3fb683777f3c51e35c5a618e313636
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-26 22:49:58 +0630

    -

--- HotelBookinSystem.WindowForm/CreatCustomerForm.cs
+++ HotelBookinSystem.WindowForm/CreatCustomerForm.cs
@@ -1,80 +1,79 @@
 ﻿using System.Data.SqlClient;
 using HotelBookinSystem.WindowForm.Config;
 
-namespace HotelBookinSystem.WindowForm
+namespace HotelBookinSystem.WindowForm;
+
+public partial class CreatCustomerForm : Form
 {
-    public partial class CreatCustomerForm : Form
+    public CreatCustomerForm()
     {
-        public CreatCustomerForm()
+        InitializeComponent();
+    }
+
+    private async void button1_Click(object sender, EventArgs e)
+    {
+        try
         {
-            InitializeComponent();
-        }
-
-        private async void button1_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                string firstName = txtFirstName.Text;
-                string lastName = txtLastName.Text;
-                string phoneNo = txtPhoneNo.Text;
-                string email = txtEmail.Text;
-                string address = txtEmail.Text;
-                string dateOfBirth = txtDateOfBirth.Text;
+            string firstName = txtFirstName.Text;
+            string lastName = txtLastName.Text;
+            string phoneNo = txtPhoneNo.Text;
+            string email = txtEmail.Text;
+            string address = txtEmail.Text;
+            string dateOfBirth = txtDateOfBirth.Text;
 
 
-                string query =
-                    @"INSERT INTO Customer_Table(FirstName, LastName, Phone, Email, Address, DateOfBirth)
+            string query =
+                @"INSERT INTO Customer_Table(FirstName, LastName, Phone, Email, Address, DateOfBirth)
                       VALUES(@FirstName, @LastName, @Phone, @Email, @Address, @DateOfBirth)";
 
-                List<SqlParameter> parameters = new()
-                {
-                    new("@FirstName",firstName),
-                    new("@LastName",lastName),
-                    new("@Phone",phoneNo),
-                    new("@Email",email),
-                    new("@Address",address),
-                    new("@DateOfBirth",dateOfBirth)
-
-                };
-
-                SqlConnection connection = new SqlConnection(DbConfig._connectionString);
-                await connection.OpenAsync();
-
-                SqlCommand command = new SqlCommand(query, connection);
-                command.Parameters.AddRange(parameters.ToArray());
-
-                int result = await command.ExecuteNonQueryAsync();
-                await connection.CloseAsync();
-
-                if (result == 1)
-                {
-                    MessageBox.Show("Saving Successful.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-                    CustomerListForm customerListForm = new();
-                    customerListForm.Show();
-                    this.Hide();
-
-                    return;
-                }
-                MessageBox.Show("Saving Fail.", "Fail", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
-
-            }
-            catch (Exception ex)
+            List<SqlParameter> parameters = new()
             {
-                throw new Exception(ex.Message);
+                new("@FirstName",firstName),
+                new("@LastName",lastName),
+                new("@Phone",phoneNo),
+                new("@Email",email),
+                new("@Address",address),
+                new("@DateOfBirth",dateOfBirth)
+
+            };
+
+            SqlConnection connection = new SqlConnection(DbConfig._connectionString);
+            await connection.OpenAsync();
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddRange(parameters.ToArray());
+
+            int result = await command.ExecuteNonQueryAsync();
+            await connection.CloseAsync();
+
+            if (result == 1)
+            {
+                MessageBox.Show("Saving Successful.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                CustomerListForm customerListForm = new();
+                customerListForm.Show();
+                this.Hide();
+
+                return;
             }
-        }
+            MessageBox.Show("Saving Fail.", "Fail", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
 
-        private void CreatCustomerForm_Load(object sender, EventArgs e)
+        }
+        catch (Exception ex)
         {
-
+            throw new Exception(ex.Message);
         }
+    }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            CustomerListForm customerListForm = new CustomerListForm();
-            customerListForm.Show();
-            this.Hide();
-        }
+    private void CreatCustomerForm_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+        CustomerListForm customerListForm = new CustomerListForm();
+        customerListForm.Show();
+        this.Hide();
     }
 }
