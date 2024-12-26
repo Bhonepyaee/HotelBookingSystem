cf9f31276ca081f804857a2a15a297a4c7d56390
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-26 22:53:03 +0630

    -

--- MVC/Models/BlogModel.cs
+++ MVC/Models/BlogModel.cs
@@ -1,10 +1,9 @@
-﻿namespace MVC.Models
+﻿namespace MVC.Models;
+
+public class BlogModel
 {
-    public class BlogModel
-    {
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
