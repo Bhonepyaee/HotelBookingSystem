16c16f35ed71f829d0f79077b0653b393465a887
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-26 22:51:22 +0630

    -

--- HotelBookinSystem.WindowForm/RoomForm.Designer.cs
+++ HotelBookinSystem.WindowForm/RoomForm.Designer.cs
@@ -46,9 +46,10 @@
             // 
             button2.Image = (Image)resources.GetObject("button2.Image");
             button2.ImageAlign = ContentAlignment.MiddleRight;
-            button2.Location = new Point(55, 60);
+            button2.Location = new Point(63, 80);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(118, 29);
+            button2.Size = new Size(135, 39);
             button2.TabIndex = 61;
             button2.Text = "Back";
             button2.UseVisualStyleBackColor = true;
@@ -57,9 +58,10 @@
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(278, 58);
+            pictureBox1.Location = new Point(318, 77);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(46, 31);
+            pictureBox1.Size = new Size(53, 41);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 60;
             pictureBox1.TabStop = false;
@@ -68,9 +70,10 @@
             // 
             button1.Image = (Image)resources.GetObject("button1.Image");
             button1.ImageAlign = ContentAlignment.MiddleRight;
-            button1.Location = new Point(654, 393);
+            button1.Location = new Point(747, 524);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(118, 29);
+            button1.Size = new Size(135, 39);
             button1.TabIndex = 59;
             button1.Text = "Create";
             button1.UseVisualStyleBackColor = true;
@@ -81,9 +84,10 @@
             txtMaxOccupants.BackColor = Color.Silver;
             txtMaxOccupants.BorderStyle = BorderStyle.None;
             txtMaxOccupants.ForeColor = Color.Black;
-            txtMaxOccupants.Location = new Point(429, 283);
+            txtMaxOccupants.Location = new Point(490, 377);
+            txtMaxOccupants.Margin = new Padding(3, 4, 3, 4);
             txtMaxOccupants.Name = "txtMaxOccupants";
-            txtMaxOccupants.Size = new Size(103, 16);
+            txtMaxOccupants.Size = new Size(118, 20);
             txtMaxOccupants.TabIndex = 54;
             // 
             // label4
@@ -91,9 +95,9 @@
             label4.AutoSize = true;
             label4.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label4.ForeColor = SystemColors.ControlDark;
-            label4.Location = new Point(313, 283);
+            label4.Location = new Point(358, 377);
             label4.Name = "label4";
-            label4.Size = new Size(96, 16);
+            label4.Size = new Size(121, 20);
             label4.TabIndex = 53;
             label4.Text = "MaxOccupants";
             label4.Click += label4_Click;
@@ -103,9 +107,10 @@
             txtRoomType.BackColor = Color.Silver;
             txtRoomType.BorderStyle = BorderStyle.None;
             txtRoomType.ForeColor = Color.Black;
-            txtRoomType.Location = new Point(429, 226);
+            txtRoomType.Location = new Point(490, 301);
+            txtRoomType.Margin = new Padding(3, 4, 3, 4);
             txtRoomType.Name = "txtRoomType";
-            txtRoomType.Size = new Size(103, 16);
+            txtRoomType.Size = new Size(118, 20);
             txtRoomType.TabIndex = 52;
             // 
             // label3
@@ -113,9 +118,9 @@
             label3.AutoSize = true;
             label3.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label3.ForeColor = SystemColors.ControlDark;
-            label3.Location = new Point(317, 226);
+            label3.Location = new Point(362, 301);
             label3.Name = "label3";
-            label3.Size = new Size(76, 16);
+            label3.Size = new Size(89, 20);
             label3.TabIndex = 51;
             label3.Text = "RoomType";
             // 
@@ -124,9 +129,10 @@
             txtRoomNumber.BackColor = Color.Silver;
             txtRoomNumber.BorderStyle = BorderStyle.None;
             txtRoomNumber.ForeColor = Color.Black;
-            txtRoomNumber.Location = new Point(429, 169);
+            txtRoomNumber.Location = new Point(490, 225);
+            txtRoomNumber.Margin = new Padding(3, 4, 3, 4);
             txtRoomNumber.Name = "txtRoomNumber";
-            txtRoomNumber.Size = new Size(103, 16);
+            txtRoomNumber.Size = new Size(118, 20);
             txtRoomNumber.TabIndex = 50;
             // 
             // label2
@@ -134,9 +140,9 @@
             label2.AutoSize = true;
             label2.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label2.ForeColor = SystemColors.ControlDark;
-            label2.Location = new Point(317, 169);
+            label2.Location = new Point(362, 225);
             label2.Name = "label2";
-            label2.Size = new Size(92, 16);
+            label2.Size = new Size(112, 20);
             label2.TabIndex = 49;
             label2.Text = "RoomNumber";
             // 
@@ -145,18 +151,18 @@
             label1.AutoSize = true;
             label1.Font = new Font("Microsoft Sans Serif", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.ActiveCaption;
-            label1.Location = new Point(339, 58);
+            label1.Location = new Point(387, 77);
             label1.Name = "label1";
-            label1.Size = new Size(206, 33);
+            label1.Size = new Size(259, 42);
             label1.TabIndex = 48;
             label1.Text = "Select ROOM";
             // 
             // RoomForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(24, 30, 54);
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
             Controls.Add(button2);
             Controls.Add(pictureBox1);
             Controls.Add(button1);
@@ -167,8 +173,10 @@
             Controls.Add(txtRoomNumber);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "RoomForm";
             Text = "RoomForm";
+            Load += RoomForm_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
--- HotelBookinSystem.WindowForm/RoomForm.cs
+++ HotelBookinSystem.WindowForm/RoomForm.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Data.SqlClient;
 using HotelBookinSystem.WindowForm.Config;
 
 namespace HotelBookinSystem.WindowForm
@@ -78,6 +69,11 @@ namespace HotelBookinSystem.WindowForm
             RoomListForm roomListForm = new RoomListForm();
             roomListForm.Show();
             this.Hide();
+        }
+
+        private void RoomForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
