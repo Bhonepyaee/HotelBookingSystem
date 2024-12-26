9427c1e70e23c81dd9658a83071ba633e320edf1
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-26 22:48:45 +0630

    -

--- HotelBookinSystem.WindowForm/Query/BookingQuery.cs
+++ HotelBookinSystem.WindowForm/Query/BookingQuery.cs
@@ -1,9 +1,8 @@
-﻿namespace HotelBookinSystem.WindowForm.Query
+﻿namespace HotelBookinSystem.WindowForm.Query;
+
+public class BookingQuery
 {
-    public class BookingQuery
-    {
-        public static string GetBookingQuery { get; } =
-            @"SELECT BookingId, CustomerId, RoomId, CheckInDate, CheckOutDate, BookingDate, Status
+    public static string GetBookingQuery { get; } =
+        @"SELECT BookingId, CustomerId, RoomId, CheckInDate, CheckOutDate, BookingDate, Status
              FROM Booking_Table ORDER BY BookingId DESC";
-    }
 }
