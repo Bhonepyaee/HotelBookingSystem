073c32d8ff463fb17c925aaa640bbde6ae44ac26
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-26 22:48:30 +0630

    -

--- HotelBookinSystem.WindowForm/Config/DbConfig.cs
+++ HotelBookinSystem.WindowForm/Config/DbConfig.cs
@@ -1,8 +1,7 @@
-﻿namespace HotelBookinSystem.WindowForm.Config
+﻿namespace HotelBookinSystem.WindowForm.Config;
+
+public class DbConfig
 {
-    public class DbConfig
-    {
-        public static string _connectionString { get; } =
-            "Server=.;DataBase=HotelBookingSystem;User ID=sa;Password=sasa@123;TrustServerCertificate=True;";
-    }
+    public static string _connectionString { get; } =
+        "Server=.;DataBase=HotelBookingSystem;User ID=sa;Password=sasa@123;TrustServerCertificate=True;";
 }
