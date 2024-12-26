bb60bb15e5447129f5b3ab88eff5b04e93aa4e87
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-26 22:48:08 +0630

    -

--- HotelBookinSystem.WindowForm/Services/AdoDotNetService.cs
+++ HotelBookinSystem.WindowForm/Services/AdoDotNetService.cs
@@ -1,12 +1,7 @@
 ﻿using HotelBookinSystem.WindowForm.Config;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HotelBookinSystem.WindowForm.Services
 {
