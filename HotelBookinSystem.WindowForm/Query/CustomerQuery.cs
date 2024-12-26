ff172bd8b8675b0c16a84510e41564eaeebcc715
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-26 22:48:51 +0630

    -

--- HotelBookinSystem.WindowForm/Query/CustomerQuery.cs
+++ HotelBookinSystem.WindowForm/Query/CustomerQuery.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HotelBookinSystem.WindowForm.Query
+﻿namespace HotelBookinSystem.WindowForm.Query
 {
     public class CustomerQuery
     {
