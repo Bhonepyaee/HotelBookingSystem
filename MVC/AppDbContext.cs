a96d9a4eef5f3b61901a27509b8e5e7f62d0e9f1
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-26 22:53:29 +0630

    -

--- MVC/AppDbContext.cs
+++ MVC/AppDbContext.cs
@@ -1,12 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MVC.DB;
 
-namespace MVC
-{
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions options) : base(options) { }
+namespace MVC;
 
-        public DbSet<Tbl_Blog> Tbl_Blogs { get; set; }
-    }
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Tbl_Blog> Tbl_Blogs { get; set; }
 }
