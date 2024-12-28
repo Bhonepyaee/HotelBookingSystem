e2ded089b533614fc6c00bd8f58c227592afe642
Lin Thit Htoo <lth1212001@gmail.com>
2024-12-28 20:12:09 +0630

    -

--- HotelBookinSystem.WindowForm/ChangePassword.cs
+++ HotelBookinSystem.WindowForm/ChangePassword.cs
@@ -1,16 +1,5 @@
-﻿using HotelBookinSystem.WindowForm.AppDbContextModels;
-using HotelBookinSystem.WindowForm.Exrtensions;
+﻿using HotelBookinSystem.WindowForm.Exrtensions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace HotelBookinSystem.WindowForm
 {
