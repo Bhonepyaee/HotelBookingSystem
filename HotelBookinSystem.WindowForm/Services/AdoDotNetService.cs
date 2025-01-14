651f098a9bec009c155ac7666dc4f444fe6aefe4
Lin Thit Htoo <lth1212001@gmail.com>
2025-01-14 19:03:00 +0630

    -

--- HotelBookinSystem.WindowForm/Services/AdoDotNetService.cs
+++ HotelBookinSystem.WindowForm/Services/AdoDotNetService.cs
@@ -35,18 +35,19 @@ public class AdoDotNetService
     //    }
     //}
 
-    public async Task<List<T>> QueryAsync<T>(string query, SqlParameter[]? parameters = null, CommandType commandType = CommandType.Text)
+    public async Task<List<T>> QueryAsync<T>(
+        string query,
+        SqlParameter[]? parameters = null,
+        CommandType commandType = CommandType.Text
+    )
     {
         try
         {
             SqlConnection connection = GetSqlConnection();
             await connection.OpenAsync();
 
-            SqlCommand command = new SqlCommand(query, connection)
-            {
-                CommandType = commandType
-            };
-            if(parameters is not null)
+            SqlCommand command = new SqlCommand(query, connection) { CommandType = commandType };
+            if (parameters is not null)
             {
                 command.Parameters.AddRange(parameters);
             }
@@ -54,7 +55,7 @@ public class AdoDotNetService
             SqlDataAdapter adapter = new SqlDataAdapter(command);
             DataTable dt = new DataTable();
             adapter.Fill(dt);
-            
+
             await connection.CloseAsync();
 
             string jsonStr = JsonConvert.SerializeObject(dt);
@@ -66,17 +67,18 @@ public class AdoDotNetService
         }
     }
 
-    public async Task<DataTable> QueryFirstOrDefaultAsync(string query, SqlParameter[]? parameters = null, CommandType commandType = CommandType.Text)
+    public async Task<DataTable> QueryFirstOrDefaultAsync(
+        string query,
+        SqlParameter[]? parameters = null,
+        CommandType commandType = CommandType.Text
+    )
     {
         try
         {
             SqlConnection connection = GetSqlConnection();
             await connection.OpenAsync();
 
-            SqlCommand command = new(query, connection)
-            {
-                CommandType = commandType
-            };
+            SqlCommand command = new(query, connection) { CommandType = commandType };
             if (parameters is not null)
             {
                 command.Parameters.AddRange(parameters);
