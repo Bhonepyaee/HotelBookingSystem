58788bd5f73cdcaa70a076955398b794a8337eb5
Lin Thit Htoo <lth1212001@gmail.com>
2025-01-15 16:55:42 +0630

    -

--- Mini_Banking/Program.cs
+++ Mini_Banking/Program.cs
@@ -17,8 +17,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
