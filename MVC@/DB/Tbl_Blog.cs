6fae0a8defb2bc117c4f6672b1c8497437ff2634
Lin Thit Htoo <lth1212001@gmail.com>
2025-01-15 16:54:22 +0630

    -

--- MVC@/DB/Tbl_Blog.cs
+++ MVC@/DB/Tbl_Blog.cs
@@ -1,15 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace MVC_.DB
+namespace MVC_.DB;
+
+[Table("Tbl_Blog")]
+public class Tbl_Blog
 {
-    [Table("Tbl_Blog")]
-    public class Tbl_Blog
-    {
-        [Key]
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-    }
+    [Key]
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
 }
