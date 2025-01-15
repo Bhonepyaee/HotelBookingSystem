7b6550b58534e9f407fcb920df8b3f439ffa241d
Lin Thit Htoo <lth1212001@gmail.com>
2025-01-15 16:55:23 +0630

    -

--- Mini_Banking/Controllers/LoginController.cs
+++ Mini_Banking/Controllers/LoginController.cs
@@ -1,46 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Mini_Banking.Controllers
+namespace Mini_Banking.Controllers;
+
+public class LoginController : Controller
 {
-    public class LoginController : Controller
+    internal readonly IConfiguration _configuration;
+
+    public LoginController(IConfiguration configuration)
     {
-        internal readonly IConfiguration _configuration;
-
-        public LoginController(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
-        [ActionName("Login")]
-        public async Task<IActionResult> Index()
-        {
-
-            #region Index
-            try
-            {
-
-                return View();
-            }
-            catch(Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        
-        }
-        #endregion
-
-        [ActionName("Register")]
-        public async Task<IActionResult> Index1()
-        {
-            try
-            {
-                return View();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
+        _configuration = configuration;
     }
+
+    [ActionName("Login")]
+    public async Task<IActionResult> Index()
+    {
+
+        #region Index
+        try
+        {
+
+            return View();
+        }
+        catch(Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    
+    }
+    #endregion
+
+    [ActionName("Register")]
+    public async Task<IActionResult> Index1()
+    {
+        try
+        {
+            return View();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
 }
