72ca28e5816c48ff11af36dd03c8a3a31663694a
Lin Thit Htoo <lth1212001@gmail.com>
2025-01-15 16:54:49 +0630

    -

--- MVC@/Queries/BlogQuery.cs
+++ MVC@/Queries/BlogQuery.cs
@@ -1,21 +1,20 @@
-﻿namespace MVC_.Queries
+﻿namespace MVC_.Queries;
+
+public class BlogQuery
 {
-    public class BlogQuery
-    {
-        public static string GetBlogListQuery { get; } =
-            @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent 
+    public static string GetBlogListQuery { get; } =
+        @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent 
              FROM Tbl_Blog WHERE BlogId = @BlogId";
 
-        public static string BlogListQuery { get; } =
-            @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent 
+    public static string BlogListQuery { get; } =
+        @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent 
              FROM Tbl_Blog ORDER BY BlogId DESC";
 
-        public static string CreateBlogQuery { get; } =
-            @"INSERT INTO Tbl_Blog(BlogTitle, BlogAuthor, BlogContent)
+    public static string CreateBlogQuery { get; } =
+        @"INSERT INTO Tbl_Blog(BlogTitle, BlogAuthor, BlogContent)
               VALUES(@BlogTitle, @BlogAuthor, @BlogContent)";
 
-        public static string UpdateBlogListQuery { get; } =
-            @"UPDATE Tbl_Blog SET BlogTitle = @BlogTitle,
+    public static string UpdateBlogListQuery { get; } =
+        @"UPDATE Tbl_Blog SET BlogTitle = @BlogTitle,
               BlogAuthor = @BlogAuthor, BlogContent = @BlogContent WHERE BlogId = @BlogId";
-    }
 }
