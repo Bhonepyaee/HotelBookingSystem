9a6fa5525152c23c8d53ed254b8209df611f20c6
Lin Thit Htoo <lth1212001@gmail.com>
2025-01-15 16:54:41 +0630

    -

--- MVC@/Models/ErrorViewModel.cs
+++ MVC@/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace MVC_.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace MVC_.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
