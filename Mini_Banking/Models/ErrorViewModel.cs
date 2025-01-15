bb1bd2f2ae7aeeffdeb9b591a1d2b03ba8f78779
Lin Thit Htoo <lth1212001@gmail.com>
2025-01-15 16:55:32 +0630

    -

--- Mini_Banking/Models/ErrorViewModel.cs
+++ Mini_Banking/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace Mini_Banking.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace Mini_Banking.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
