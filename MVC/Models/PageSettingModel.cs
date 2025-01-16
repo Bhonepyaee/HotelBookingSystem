3f05b48ffd5a0b3bda6fec1cbdfe716f1013b8ad
Lin Thit Htoo <lth1212001@gmail.com>
2025-01-16 08:48:00 +0630

    -

--- MVC/Models/PageSettingModel.cs
+++ MVC/Models/PageSettingModel.cs
@@ -2,7 +2,13 @@
 
 public class PageSettingModel
 {
-    public PageSettingModel(int pageNo, int pageSize, int pageCount, int pageRowCount, string pageUrl)
+    public PageSettingModel(
+        int pageNo,
+        int pageSize,
+        int pageCount,
+        int pageRowCount,
+        string pageUrl
+    )
     {
         PageNo = pageNo;
         PageSize = pageSize;
@@ -11,10 +17,9 @@ public class PageSettingModel
         PageUrl = pageUrl;
     }
 
-   public int PageNo { get; set; }
-   public int PageSize { get; set; }
-   public int PageCount { get; set; }
-   public int PageRowCount { get; set; }
-   public string PageUrl { get; set; }
-
+    public int PageNo { get; set; }
+    public int PageSize { get; set; }
+    public int PageCount { get; set; }
+    public int PageRowCount { get; set; }
+    public string PageUrl { get; set; }
 }
